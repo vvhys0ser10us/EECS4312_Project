e841730e4907952ad0abf46a4a5ce841a8318282
Haoran Zhou <zhouk@my.yorku.ca>
2020-12-03 14:27:49 -0500

    Update paid.java
    add countdown function.

--- code/Parking/src/paid.java
+++ code/Parking/src/paid.java
@@ -1,12 +1,15 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 public class paid {
 	private JFrame frame;
+	private  Timer timer;
+	int counter = 10;
 	
 	public paid() {
 		frame = new JFrame();
@@ -18,8 +21,41 @@ public class paid {
 		frame.setVisible(true);
 		
 		JLabel paymentLabel = new JLabel("Thanks for the payment!");
-		paymentLabel.setBounds(150, 200, 200, 16);
+		paymentLabel.setBounds(150, 150, 200, 16);
 		frame.getContentPane().add(paymentLabel);
+		
+		JLabel countdown = new JLabel("Parking space expiry in  ");
+		countdown.setBounds(115, 200, 200, 16);
+		frame.getContentPane().add(countdown);
+		
+		JLabel t = new JLabel();
+		t.setBounds(255, 200, 200, 16);
+		frame.getContentPane().add(t);
+		
+		JLabel s = new JLabel("seconds");
+		s.setBounds(270, 200, 200, 16);
+		frame.getContentPane().add(s);
+		
+		
+		 timer = new Timer(1000, new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                t.setText(String.valueOf(counter));
+	                counter--;
+	                if (counter < 0) {
+	                    //timer.removeActionListener(this);
+	                      timer.stop();
+	                      welcome.currentBooking.completedStatus();
+	                      welcome.currentBooking.getSpace().setOccupited(false);
+	                      JOptionPane.showMessageDialog(null, "Your parking space is expiry!");
+	                }
+	            }
+	        });
+	        timer.start();
+		
+		
+		
+	
+		
 		
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(new ActionListener() {
